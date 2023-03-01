dc064f6846d6c7b5c995f40e519b6a4d04871f6c
Ere Mid <eremid@gmail.com>
2023-03-01 11:39:07 +0100

    feat: valid ip with net.ParseIP

---
+++ stringvalidator/valid_ip.go
@@ -0,0 +1,50 @@
+package stringvalidator
+
+import (
+	"context"
+	"fmt"
+	"net"
+
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+)
+
+var _ validator.String = netIPValidator{}
+
+type netIPValidator struct {
+}
+
+// Description describes the validation in plain text formatting.
+func (validator netIPValidator) Description(_ context.Context) string {
+	return "must be a valid IP with net.ParseIP"
+}
+
+// MarkdownDescription describes the validation in Markdown formatting.
+func (validator netIPValidator) MarkdownDescription(ctx context.Context) string {
+	return validator.Description(ctx)
+}
+
+// Validate performs the validation.
+func (validator netIPValidator) ValidateString(
+	ctx context.Context,
+	request validator.StringRequest,
+	response *validator.StringResponse,
+) {
+	if request.ConfigValue.IsNull() || request.ConfigValue.IsUnknown() {
+		return
+	}
+
+	if net.ParseIP(request.ConfigValue.ValueString()) == nil {
+		response.Diagnostics.AddAttributeError(
+			request.Path,
+			"Failed to parse IP address",
+			fmt.Sprintf("invalid value: %s", request.ConfigValue.String()),
+		)
+	}
+}
+
+// IsValidIP returns a validator which ensures that the configured attribute
+// value is a valid IP address with net.ParseIP package.
+// Null (unconfigured) and unknown (known after apply) values are skipped.
+func IsValidIP() validator.String {
+	return &netIPValidator{}
+}
