e4984f6f2b524825083458ff1297e7d3d3d16754
Mickael Stanislas <contact@mickael-stanislas.com>
2023-03-10 17:42:03 +0100

    refactor: refactor funcs use the regex for validate

--- stringvalidator/valid_urn.go
+++ stringvalidator/valid_urn.go
@@ -1,50 +1,19 @@
 package stringvalidator
 
 import (
-	"context"
-	"fmt"
-	"regexp"
-
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+
+	"github.com/FrangipaneTeam/terraform-plugin-framework-validators/docs/stringvalidator/common"
 )
 
-var _ validator.String = urnValidator{}
-
-type urnValidator struct{}
-
-// Description describes the validation in plain text formatting.
-func (validator urnValidator) Description(_ context.Context) string {
-	return "must be a valid URN"
-}
-
-// MarkdownDescription describes the validation in Markdown formatting.
-func (validator urnValidator) MarkdownDescription(ctx context.Context) string {
-	return validator.Description(ctx)
-}
-
-// Validate performs the validation.
-func (validator urnValidator) ValidateString(
-	ctx context.Context,
-	request validator.StringRequest,
-	response *validator.StringResponse,
-) {
-	if request.ConfigValue.IsNull() || request.ConfigValue.IsUnknown() {
-		return
-	}
-
-	re := regexp.MustCompile(`(?m)urn:[A-Za-z0-9][A-Za-z0-9-]{0,31}:([A-Za-z0-9()+,\-.:=@;$_!*']|%[0-9A-Fa-f]{2})+`)
-
-	if !re.MatchString(request.ConfigValue.ValueString()) {
-		response.Diagnostics.AddAttributeError(
-			request.Path,
-			"Failed to parse URN",
-			fmt.Sprintf("invalid URN: %s", request.ConfigValue.String()),
-		)
-	}
-}
-
 // IsValidURN returns a validator which ensures that the configured attribute
+// value is a valid URN.
 // Null (unconfigured) and unknown (known after apply) values are skipped.
 func IsValidURN() validator.String {
-	return &urnValidator{}
+	return &common.RegexValidator{
+		Desc:         "must be a valid URN",
+		Regex:        `(?m)urn:[A-Za-z0-9][A-Za-z0-9-]{0,31}:([A-Za-z0-9()+,\-.:=@;$_!*']|%[0-9A-Fa-f]{2})+`,
+		ErrorSummary: "Failed to parse URN",
+		ErrorDetail:  "This value is not a valid URN",
+	}
 }
--- stringvalidator/valid_uuid.go
+++ stringvalidator/valid_uuid.go
@@ -1,51 +1,19 @@
 package stringvalidator
 
 import (
-	"context"
-	"fmt"
-	"regexp"
-
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
+
+	"github.com/FrangipaneTeam/terraform-plugin-framework-validators/docs/stringvalidator/common"
 )
-
-var _ validator.String = uuidValidator{}
-
-type uuidValidator struct{}
-
-// Description describes the validation in plain text formatting.
-func (validator uuidValidator) Description(_ context.Context) string {
-	return "must be a valid UUID"
-}
-
-// MarkdownDescription describes the validation in Markdown formatting.
-func (validator uuidValidator) MarkdownDescription(ctx context.Context) string {
-	return validator.Description(ctx)
-}
-
-// Validate performs the validation.
-func (validator uuidValidator) ValidateString(
-	ctx context.Context,
-	request validator.StringRequest,
-	response *validator.StringResponse,
-) {
-	if request.ConfigValue.IsNull() || request.ConfigValue.IsUnknown() {
-		return
-	}
-
-	re := regexp.MustCompile(`(?m)^\w{8}-\w{4}-\w{4}-\w{4}-\w{12}$`)
-
-	if !re.MatchString(request.ConfigValue.ValueString()) {
-		response.Diagnostics.AddAttributeError(
-			request.Path,
-			"Failed to parse UUID",
-			fmt.Sprintf("invalid UUID: %s", request.ConfigValue.String()),
-		)
-	}
-}
 
 // IsValidUUID returns a validator which ensures that the configured attribute
 // value is a valid (v4) UUID.
 // Null (unconfigured) and unknown (known after apply) values are skipped.
 func IsValidUUID() validator.String {
-	return &uuidValidator{}
+	return &common.RegexValidator{
+		Desc:         "must be a valid UUID",
+		Regex:        `(?m)^\w{8}-\w{4}-\w{4}-\w{4}-\w{12}$`,
+		ErrorSummary: "Failed to parse UUID",
+		ErrorDetail:  "This value is not a valid (v4) UUID",
+	}
 }
