41ed4a9aa21c65fe74406587d0f6e192e2e8a49d
Mickael Stanislas <contact@mickael-stanislas.com>
2023-03-10 19:49:07 +0100

    feat: add `Not` bool validator

---
+++ boolvalidator/not_valid_test.go
@@ -0,0 +1,10 @@
+package boolvalidator_test
+
+import (
+	"testing"
+)
+
+func TestNotValidator(t *testing.T) {
+	// Actually is not possible to test NotValidator because the other validators
+	// use schema validation.
+}
